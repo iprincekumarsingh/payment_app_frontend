02e85eb942100e67e100f5be2391be4443ccb935
Prince Kumar Singh <princekumar2000.pks@gmail.com>
2023-04-29 04:05:01 +0530

    login type number

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
                 <input
                   id="email"
                   name="email"
-                  type="email"
+                  type="number"
                   autoComplete="email"
                   required
                   className="block w-full leading-7 p-1 rounded-md border-0 py-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
